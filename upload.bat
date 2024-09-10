f76c0b309e2d364acfca039149bc6eb0c97ae383
8emezzo <8emezzo@gmail.com>
2024-09-10 12:27:25 +0200

    Aggiornamento automatico da Scheduer Windows SRV

--- upload.bat
+++ upload.bat
@@ -1,5 +1,5 @@
 REM Naviga nella cartella del progetto
-Set-Location "C:\Users\simone.venturin\Dropbox\CAAF\config\Bot\p\8emezzo.github.io"
+REM Set-Location "C:\Users\simone.venturin\Dropbox\CAAF\config\Bot\p\8emezzo.github.io"
 
 REM Esegui git pull per aggiornare la cartella locale dai cambiamenti remoti
 git pull origin main
@@ -8,8 +8,7 @@ REM Aggiungi tutte le modifiche alla stage area
 git add .
 
 REM Commit delle modifiche con un messaggio generico (puoi personalizzarlo)
-$commitMessage = "Aggiornamento automatico"
-git commit -m $commitMessage
+git commit -m "Aggiornamento automatico da Scheduer Windows SRV"
 
 REM Push delle modifiche sul repository remoto
 git push origin main
